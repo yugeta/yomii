e92b5c73fc6894e62b43f232fe9000a2678df6bf
Yugeta.Koji <geta1972@gmail.com>
2023-11-26 09:34:28 +0900

    book : view

--- public/page/book/js/book.js
+++ public/page/book/js/book.js
@@ -58,8 +58,31 @@ export class Book{
     List.set_active()
   }
 
+  static clear_current_page(){
+    const page = Common.area.querySelector(`.page`)
+    if(!page){return}
+    page.parentNode.removeChild(page)
+  }
+
   static view_page(go_group_num){
-    console.log(go_group_num)
+    if(Common.group_num === go_group_num){return}
+    Book.clear_current_page()
+    Common.group_num = go_group_num ?? Common.group_num
+    const images = Book.get_group_images(Common.group_num)
+    if(!images || !images.length){return}
+    const page = document.createElement("div")
+    page.className = "page"
+    page.setAttribute("data-group" , Common.group_num)
+    for(const img_list of images){
+      const img_book = new Image()
+      img_book.src = img_list.src
+      page.appendChild(img_book)
+    }
+    Common.area.appendChild(page)
+  }
+
+  static get_group_images(group_num){
+    return Common.list.querySelectorAll(`.group[data-group="${group_num}"] .page img`)
   }
 
   // static view_page(go_page_num){
