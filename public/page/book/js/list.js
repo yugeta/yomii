68066ee9bf1a1961ee52c90283ee2ae00355f6c6
Yugeta.Koji <geta1972@gmail.com>
2023-12-01 21:16:15 +0900

    list : group,page

--- public/page/book/js/list.js
+++ public/page/book/js/list.js
@@ -65,46 +65,62 @@ export class List{
   }
 
   click_list(e){
-    const elm = e.target.closest(`.group`)
-    if(!elm){return}
-    const group_num = elm.getAttribute("data-group")
-    if(!group_num || Common.group_num === Number(group_num)){return}
-    Book.view_page(Number(group_num))
-    List.set_active()
+    if(Book.is_portrait){
+      const elm = e.target.closest(`.page`)
+      if(!elm){return}
+      const page_num = elm.getAttribute("data-page-num")
+      if(!page_num || Common.page_num === Number(page_num)){return}
+      Book.view_page(Number(page_num))
+      Common.group_num = Book.get_page2group_num(page_num)
+      Common.page_num  = page_num
+      List.set_active(page_num)
+    }
+    else{
+      const elm = e.target.closest(`.group`)
+      if(!elm){return}
+      const group_num = elm.getAttribute("data-group")
+      if(!group_num || Common.group_num === Number(group_num)){return}
+      Book.view_group(Number(group_num))
+      Common.group_num = group_num
+      Common.page_num  = Book.get_group2page_num(group_num)
+      List.set_active()
+    }
   }
-
 
   static set_active(){
     if(Book.is_portrait){
       List.set_active_page(Common.page_num)
-      List.set_active_group()
+      List.set_active_group(Common.group_num)
+      setTimeout(List.set_center_page , 0)
     }
     else{
-      List.set_active_page()
+      List.set_active_page(Common.page_num)
       List.set_active_group(Common.group_num)
+      setTimeout(List.set_center_group , 0)
     }
   }
 
   static set_active_page(page_num){
+    page_num = page_num || 0
     const pages = Common.list.querySelectorAll(`.page`)
     for(const page of pages){
-      if(Number(page.getAttribute("data-group")) === Number(page_num)){
+      // console.log(page.getAttribute("data-page-num"),page_num)
+      if(Number(page.getAttribute("data-page-num")) === Number(page_num)){
         page.setAttribute("data-status","active")
       }
       else{
         page.setAttribute("data-status","passive")
       }
     }
-    setTimeout(List.set_center_group , 0)
   }
 
   static set_center_page(){
+    Common.page_num = Common.page_num || 0
     try{
-      const book_list = Common.list
-      const page_elm  = book_list.querySelector(`[data-page-num="${Common.page_num}"]`)
+      const page_elm  = Common.list.querySelector(`.page[data-page-num="${Common.page_num}"]`)
       const left      = page_elm.offsetLeft
       const width     = page_elm.offsetWidth
-      const view_w    = book_list.offsetWidth
+      const view_w    = Common.list.offsetWidth
       const center    = left + width/2 - view_w/2
       Common.list.scrollLeft = center
     }
@@ -115,7 +131,6 @@ export class List{
 
   static set_active_group(group_num){
     const groups = Common.list.querySelectorAll(`.group`)
-    console.log(group_num)
     for(const group of groups){
       if(Number(group.getAttribute("data-group")) === Number(group_num)){
         group.setAttribute("data-status","active")
@@ -124,13 +139,13 @@ export class List{
         group.setAttribute("data-status","passive")
       }
     }
-    setTimeout(List.set_center_group , 0)
   }
 
   static set_center_group(){
+    Common.group_num = Common.group_num || 0
     try{
       const book_list = Common.list
-      const page_elm  = book_list.querySelector(`[data-group="${Common.group_num}"]`)
+      const page_elm  = book_list.querySelector(`.group[data-group="${Common.group_num}"]`)
       const left      = page_elm.offsetLeft
       const width     = page_elm.offsetWidth
       const view_w    = book_list.offsetWidth
